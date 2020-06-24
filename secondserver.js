5dcad6ae2ac865531d41559f86391439beea5757
Samantha Lee <sammialicelee@yahoo.com>
2020-06-24 10:51:48 -0400

    dynamic server

--- secondserver.js
+++ secondserver.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+var PORT = process.env.PORT || 3000;
+
 app.get("/", function (req, res) {
   res.send("Testing, testing 1234....");
 });
